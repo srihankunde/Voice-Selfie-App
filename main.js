37e3e92f03eeb424fa9d45f8a54cd1fddcc4cfe5
srihankunde <90851633+srihankunde@users.noreply.github.com>
2022-03-05 16:56:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ recognition.onresult = function(event){
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
+    if(content == "take my selfie"){
+     console.log("taking selfie");
     speak();
+    }
 
 }
 
@@ -21,13 +24,20 @@ function speak(){
 
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterthis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterthis);
 
     Webcam.attach(camera);
+    setTimeout(function()  {
+
+        take_snapshot();
+        save();
+
+        
+    }, 5000);
 
     
 
@@ -43,3 +53,18 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='selfie_image'  src='"+data_uri+"'>";
+    });
+}
+
+function save(){
+
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+
+}
